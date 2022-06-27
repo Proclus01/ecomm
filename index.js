918238b58a64e26292cdc2378fa6c4d73cdadd99
Neven <95269427+Proclus01@users.noreply.github.com>
2022-06-26 21:00:27 -0700

    web server setup

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-//jshint esversion:6
-const express = require("express");
+//jshint esversion:11
+import express from 'express';
 
-// Add something in here
+const app = express();
 
+//              // ************************ //
+//              //                          //
+//              //      Route Handler       //
+//              //                          //
+//              // ************************ //
+
+app.get('/', (req, res) => {
+    // send string to whoever makes a request to the root route
+    res.send('hi there');
+});
+
+//              // ************************ //
+//              //                          //
+//              //        Listener          //
+//              //                          //
+//              // ************************ //
+
+app.listen(3000, () => {
+    // listen for messages at this port
+    console.log('listening on port 3000');
+});
