f7d9661f68422149c29f4a30baf917b86b647b1d
Neven <95269427+Proclus01@users.noreply.github.com>
2022-06-26 23:49:47 -0700

    refactor body-parser for reuse

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ import bodyParser from 'body-parser';
 
 const app = express();
 
+// Automatically use body-parser in every route handler in this file
+app.use(bodyParser.urlencoded({ extended: true })); 
+
 //     // ************************ //       //
 //     //                          //       //
 //     //      Route Handlers      //       //
@@ -29,7 +32,9 @@ app.get('/', (req, res) => {
             <form method="POST">
                 <input name="email" placeholder="email" />
                 <input name="password" placeholder="password" />
-                <input name="passwordConfirmation" placeholder="password confirmation" />
+                <input 
+                    name="passwordConfirmation" 
+                    placeholder="password confirmation" />
                 <button>Sign Up</button>
             </form>
         </div>
@@ -40,7 +45,7 @@ app.get('/', (req, res) => {
 
 // pass in bodyParser method to parameters in app.post to parse our data
 
-app.post('/', bodyParser.urlencoded({ extended: true }), (req, res) => {
+app.post('/', (req, res) => {
     // Access attributes of email, password, passwordConfirmation in form
     // Save these attributes as user data
     console.log(req.body);
