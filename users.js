cc12ea5df7c3fb6340912f3d0e5f5df9ad1c0857
Neven <95269427+Proclus01@users.noreply.github.com>
2022-06-28 23:59:55 -0700

    added initial validation

--- users.js
+++ users.js
@@ -3,5 +3,17 @@ class UsersRepository {
         if (!filename) {
             throw new Error('Creating a repository requires a filename');
         }
+
+        // Store input as an instance variable
+        this.filename = filename; 
+
+        // Check to see if the file exists
+        // and make a new file if it doesn't
+        try {
+            fs.accessSync(this.filename);
+        } catch (err) {
+            
+        }
+        
     }
 }
