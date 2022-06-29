1c6f894335d49b957dc8fab321c3b19b09d1e8b9
Neven <95269427+Proclus01@users.noreply.github.com>
2022-06-29 00:02:53 -0700

    finalizing validation in constructor

--- users.js
+++ users.js
@@ -1,5 +1,8 @@
+import fs from 'fs';
+
 class UsersRepository {
     constructor(filename) {
+        // 
         if (!filename) {
             throw new Error('Creating a repository requires a filename');
         }
@@ -10,9 +13,10 @@ class UsersRepository {
         // Check to see if the file exists
         // and make a new file if it doesn't
         try {
+            // Use accessSync in constructor since async doesn't work here
             fs.accessSync(this.filename);
         } catch (err) {
-            
+            fs.writeFileSync(this.filename, '[]'); // Same for writeFileSync
         }
         
     }
