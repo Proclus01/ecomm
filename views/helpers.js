ea68c91aa39402afaf1f9a066249dbd004026919
Neven <95269427+Proclus01@users.noreply.github.com>
2022-08-11 22:48:30 -0700

    add helpers.js file to ./views to store helper functions

---
+++ views/helpers.js
@@ -0,0 +1,11 @@
+const helpers = {
+    getError(errors, prop) {
+        try {
+            return errors.mapped()[prop].msg;
+        } catch (err) {
+            return '';
+        }
+    }
+};
+
+export default helpers;
