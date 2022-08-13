2692b625e2ec1e5d4fc4c7d4d55f1f949bfbbacd
Neven <95269427+Proclus01@users.noreply.github.com>
2022-08-12 23:19:27 -0700

    add new validators for product creation

--- routes/admin/validators.js
+++ routes/admin/validators.js
@@ -2,6 +2,15 @@ import { check } from "express-validator";
 import usersRepo from "../../repositories/users.js";
 
 const validatorChain = {
+  requireTitle: check('title')
+    .trim()
+    .isLength({ min: 5, max: 40 }),
+
+  requirePrice: check('price')
+    .trim()
+    .toFloat()
+    .isFloat({ min: 1 }), // converts even erroneous values (char) to a float anyways (to 1)
+
   requireEmail: check("email")
     .trim()
     .normalizeEmail()
