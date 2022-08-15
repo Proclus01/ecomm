6f9cdb2d4c5f8d9c9465358e19854f2d2f7bfd0e
Neven <95269427+Proclus01@users.noreply.github.com>
2022-08-14 22:52:08 -0700

    add image editing input

--- views/admin/products/edit.js
+++ views/admin/products/edit.js
@@ -5,7 +5,8 @@ const productsEditTemplate = ({ product }) => {
         content: `
             <form method="POST">
                 <input name="title" value="${product.title}" />
-                <input name="title" value="${product.price}" />
+                <input name="price" value="${product.price}" />
+                <input name="image" type="file" />
                 <button>Submit</button>
             </form>
         `
