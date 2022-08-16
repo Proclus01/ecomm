d80f152d75bac93f156b6225be71d8d0cd80c2da
Neven <95269427+Proclus01@users.noreply.github.com>
2022-08-15 23:37:18 -0700

    update styling to edit form

--- views/admin/products/edit.js
+++ views/admin/products/edit.js
@@ -4,15 +4,37 @@ import layout from '../layout.js';
 const productsEditTemplate = ({ product, errors }) => {
     return layout({
         content: `
-            <form method="POST">
-                <input name="title" value="${product.title}" />
-                ${helpers.getError(errors, 'title')}
-                <input name="price" value="${product.price}" />
-                ${helpers.getError(errors, 'price')}
-                <input name="image" type="file" />
-                <button>Submit</button>
+        <div class="columns is-centered">
+          <div class="column is-half">
+            <h1 class="subtitle">Edit a Product</h1>
+  
+            <form method="POST" enctype="multipart/form-data">
+              <div class="field">
+                <label class="label">Title</label>
+                <input value="${
+                  product.title
+                }" class="input" placeholder="Title" name="title">
+                <p class="help is-danger">${helpers.getError(errors, 'title')}</p>
+              </div>
+              
+              <div class="field">
+                <label class="label">Price</label>
+                <input value="${
+                  product.price
+                }" class="input" placeholder="Price" name="price">
+                <p class="help is-danger">${helpers.getError(errors, 'price')}</p>
+              </div>
+              
+              <div class="field">
+                <label class="label">Image</label>            
+                <input type="file" name="image" />
+              </div>
+              <br />
+              <button class="button is-primary">Edit</button>
             </form>
-        `
+          </div>
+        </div>
+      `
     });
 };
 
