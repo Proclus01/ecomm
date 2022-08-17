4bfbeabf9d1aef953b19cf9bcbc2a1c287bda4d1
Neven <95269427+Proclus01@users.noreply.github.com>
2022-08-16 23:43:31 -0700

    add notes to structure route creation, add hidden input to views/products/index.js so card can post product id information to carts router

--- routes/carts.js
+++ routes/carts.js
@@ -4,6 +4,24 @@ import CartsRepo from '../repositories/carts.js';
 
 const router = express.Router();
 
+// Receive a post request to add an item to a cart
+
+// /cart/products
+
+// router.get(
+//     '/carts',
+//     async (req, res) => {
+//         const products = await CartsRepo.getAll();
+
+//         // send a carts template
+//     }
+// );
+
+
+// Receive a GET request to show all items in cart
+
+// Receive a post request to delete an item from a cart
+
 // router.get(
 //     '/carts',
 //     async (req, res) => {
